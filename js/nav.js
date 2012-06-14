2afe26600101a4ab7840f6ca782aa5c9ab515078
Mad Jon <biting_duck@hotmail.com>
2012-06-14 16:36:37 -0600

    update

--- js/nav.js
+++ js/nav.js
@@ -11,7 +11,7 @@ Nav.init = (typeof (Nav.init) == "undefined" || !Nav.init)
  ? function () {
 		$('ul.dropdown-nav li').hover(function(){
 			clearTimeout(Nav.t);
-			$(this).parents('ul').addClass('hover');
+			$(this).parents('.dropdown-nav').addClass('hover');
 			$(this).addClass('hover').siblings().removeClass('siblings');
 			// Nav.drop();
 			console.log(' li hover');
@@ -35,7 +35,6 @@ Nav.init = (typeof (Nav.init) == "undefined" || !Nav.init)
 			$(this).data('height', $(this).height());
 			$(this).height(0);
 		}).hover(function(){
-			console.log('hover ul(clear) ' + Nav.t);
 			clearTimeout(Nav.t);
 			$(this).stop().animate({
 				'height': $(this).data('height')
@@ -51,49 +50,6 @@ Nav.init = (typeof (Nav.init) == "undefined" || !Nav.init)
 	
 } : Nav.init;
 
-// 
-// var Nav = {
-// 	t: 0, //setTimeout()
-// 	init: function()
-// 	{
-// 		$('ul.primary-nav li.half-pint').hover(function(){
-// 			clearTimeout(Nav.t);
-// 			$(this).addClass('hover').siblings().removeClass('siblings');
-// 			// Nav.drop();
-// 			console.log(' li hover');
-// 			var $ul = $(this).children('ul');
-// 			Nav.t = setTimeout(function(){
-// 				$ul.stop().animate({
-// 					'height': $ul.data('height')
-// 				});
-// 			}, 100)
-// 		}, function(){
-// 			console.log(' li off');
-// 			var $ul = $(this).children('ul');
-// 			Nav.t = setTimeout(function(){
-// 				$ul.stop().animate({
-// 					'height': 0
-// 				});
-// 			}, 100)
-// 		});
-// 		
-// 		$('ul.primary-nav ul').each(function(){
-// 			$(this).data('height', $(this).height());
-// 			$(this).height(0);
-// 		}).hover(function(){
-// 			console.log('hover ul(clear) ' + Nav.t);
-// 			clearTimeout(Nav.t);
-// 			$(this).stop().animate({
-// 				'height': $(this).data('height')
-// 			});
-// 		}, function(){
-// 			console.log(' off ul')
-// 			Nav.t = setTimeout(function(){
-// 				$(this).stop().animate({'height': 0});
-// 			}, 100);
-// 		});
-// 	}
-// }
 $(function(){
 	Nav.init();
 })
