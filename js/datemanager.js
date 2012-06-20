f54286084a16088c29e9297587cb53f074b665aa
Jonathan Troncoso <biting_duck@Jonathans-MacBook-Air.local>
2012-06-20 10:26:25 -0600

    update

--- js/datemanager.js
+++ js/datemanager.js
@@ -10,7 +10,6 @@
 			var selectedIndex = settings.yearsToShow.indexOf(settings.selectedYear);
 			var size = (index == selectedIndex)?'large':(index == selectedIndex-1 || index == selectedIndex+1)?'small':'gone';
 			// selected year is against a wall, which means it will have 2 smalls on 1 side instead of 1 small on both sides
-			console.log(size, total, index, selectedIndex);
 			if(size == 'gone' && total > 2 && ((total-1 == selectedIndex && index == selectedIndex-2) || (selectedIndex == 0 && index == selectedIndex+2)))
 			{
 				size = 'small';
@@ -19,9 +18,12 @@
 		}
 		
 		var methods = {
+			/*
+			 * INIT!
+			 */
 			init: function(options)
 			{
-				console.log('init');
+				console.log('init options: %o', options);
 				var colors = ['blue', 'blue-green', 'green'];
 				var thisYear = new Date().getFullYear();
 
@@ -31,7 +33,6 @@
 					'selectedYear' : thisYear,
 					'yearsToShow' : [thisYear]
 				}, options);
-				console.log("datePicker settings: %o", settings);				
 				
 				return this.each(function() {
 					var $this = $(this);
@@ -40,32 +41,47 @@
 					var selectedIndex = settings.yearsToShow.indexOf(settings.selectedYear);
 					// bind click events on years
 					$.each(settings.yearsToShow, function(index, value) {
-						console.log(settings);
 						size = generateSize(index, settings);
-						console.log(size);
 						var $a = $('<a href="#"></a>').html(value.toString());
 						var $el = $("<li></li>").addClass(colors[parseInt(value)%3]).addClass(size).html($a);
-						$a.click(function() { settings.onChange(value); return false; });
+						$a.click(function() { console.log(settings); settings.onChange(value); return false; });
 						years.append($el);
 					});
+					// bind Events on left/right arrows
+					$('.left-selector, .right-selector').click(function(){
+						var eq = $(this).hasClass('left-selector')?0:2;
+						var year = $this.find('.year-selector ul li:not(.gone):eq(' + eq + ') a').html();
+						$.bbq.pushState({ "year": year });
+						return false;
+					})
 					$yearSelector.append(years);
 				});
 				
 			},
+			/**
+			 * This function selects a year (makes it largest, and makes appropriate siblings visible)
+			 */
 			selectYear: function(year)
 			{
-				settings.selectedYear = year;
 				console.log(year);
+				settings.selectedYear = parseInt(year);
+				console.log(settings);
 				return $(this).each(function(){
-					console.log('HEY!')
 					var selectedIndex = settings.yearsToShow.indexOf(settings.selectedYear);
 					var $ul = $(this).find('.year-selector ul li');
 					$ul.each(function(i,e){
 						$(e).removeClass('gone').removeClass('large').removeClass('small').addClass(generateSize(i, settings));
-						console.log(e);
-						console.log(generateSize(i, settings));
 					});
 				});
+			},
+			/**
+			 * Respond to hashChange
+			 * @note this is assuming only 1 date-manager. serious tweaks would be needed here for multiple
+			 */
+			hashChange: function()
+			{
+				var year = $.deparam.fragment().year || Date.getFullYear();
+				$('.date-manager').yearPicker('selectYear', year);
 			}
 		};
 		
@@ -79,99 +95,3 @@
     }
 	};
 })(jQuery);
-// // Declare the DateManager object.
-// var DateManager = (typeof (DateManager) == "undefined" || !DateManager)
-//  ? function () { }
-//  : DateManager;
-// 
-// // init(); bind the appropriate events
-// DateManager.init = (typeof (DateManager.init) == "undefined" || !DateManager.init)
-// 	? function () {
-// 		// Bind Click Events
-// 		$('.left-selector, .right-selector').click(function(){
-// 			DateManager.moveYears($(this).hasClass('left-selector')?true:false);
-// 			return false;
-// 		});
-// 		console.log('Initing');
-// 		$('.year-selector ul li a').live('click', function(){
-// 			$(this).parent().addClass('active').siblings().removeClass('active');
-// 			DateManager.selectDate();
-// 			return false;
-// 		});
-// 		
-// 		// Bind select Events
-// 		$('select#location, select#status').change(DateManager.selectDate);
-// 		
-// 		// Respond to hash on page load
-// 		if(window.location.hash)
-// 		{
-// 			// code...
-// 		}
-// 	} : DateManager.init;
-// 
-// // hashChange Event
-// DateManager.hashChange = (typeof (DateManager.hashChange) == "undefined" || !DateManager.hashChange)
-// 	? function () {
-// 		var $accordion = $('.accordion[rel=' + window.location.hash + ']');
-// 		if($accordion.length == 0)
-// 		{
-// 			$.getJSON('request-history.json', DateManager.replaceAccordion);
-// 		}
-// 		else
-// 		{
-// 			$accordion.siblings().fadeOut(function(){
-// 				$accordion.fadeIn();
-// 			});
-// 		}
-// 	} : DateManager.hashChange;
-// 
-// // This Finds the date and changes the hash of the url.
-// DateManager.selectDate = (typeof (DateManager.selectDate) == "undefined" || !DateManager.selectDate)
-//  ? function () {
-// 		var year = parseInt($('.year-selector ul li.active a').html());
-// 		var month = ($('.month-manager').length == 0)?false:($('.month-manager ul li.active a').length == 0)?$('.month-manager ul li.active a').html():false;
-// 		var location = $('select#location').val();
-// 		var status = $('select#status').val();
-// 		window.location.hash = status + "-" + location + "-" + year + (month == false ? '' : "-" + month);
-// 	} : DateManager.selectDate;
-// 	
-// // move years back/forth
-// DateManager.moveYears = (typeof (DateManager.moveYears) == "undefined" || !DateManager.moveYears)
-//  ? function (older) {
-// 		var right = parseInt($('.year-selector .slider').css('right').replace('px', ''));
-// 		var move;
-// 		$('.year-selector .slider').stop( false, true);
-// 		if(older == false && right < 0)
-// 		{
-// 			move = "+=247";
-// 		}
-// 		else if(older == true)
-// 		{
-// 			// need an extra year. add it before we move slide
-// 			if(($('.year-selector ul').width()+247) > (741-right))
-// 			{
-// 				var year = (parseInt($('.year-selector ul li:first a').html())-1);
-// 				var color = $('.year-selector ul li:first a').hasClass('blue')?'green':$('.year-selector ul li:first a').hasClass('blue-green')?'blue':'blue-green';
-// 				$('<li></li>')
-// 					.addClass('year-' + year)
-// 					.html('<a href="#" class="' + color + '">' + year + '</a></li>')
-// 					.prependTo($('.year-selector ul'));
-// 			}
-// 			move = "-=247";
-// 		}
-// 		else
-// 		{
-// 			move = "0";
-// 		}
-// 		$('.year-selector .slider').animate({
-// 			right: move
-// 		});
-// 		return false;
-// } : DateManager.moveYears;
-// 
-// // bind hashChange (doesn't need to be onload)
-// $(window).bind('hashchange', DateManager.hashChange);
-// $(function(){
-// 	console.log('INIT!');
-// 	DateManager.init();
-// });
--- js/request-history.js
+++ js/request-history.js
@@ -1,72 +1,79 @@
-var requestHistory = requestHistory || (function ($) {
-
-	// other requestHistory stuff omitted for this example...
-
-	// service request history stuff
-	this.history = function (options) {
-
-		// ok for this servicerequest.js to know about the hash mechanism, yearpicker.js can remain blissfully ignorant
-		// $.bbq.pushState triggers the hashchange, which ultimately updates the service request history items view
-		this.history.onYearChange = function (year) {
-			$.bbq.pushState({ "year": year });
-		};
-
-		// filters *could* be generalized into a jQuery plugin, but remain in this servicerequest.js just to illustrate they can
-		this.history.onFilterChange = function (filter) {
-			var val = filter.children("option:selected").val();
-			switch (filter.attr("id")) {
-				case options.filterTypeElement.attr("id"):
-					$.bbq.pushState({ "type": val });
-					break;
-				case options.filterStatusElement.attr("id"):
-					$.bbq.pushState({ "status": val });
-					break;
-			}
-		};
-
-		// set selected filter items from state
-		this.history.setFilterState = function (state) {
-			options.filtersElement.children("select").each(function (index, value) {
-				switch ($(value).attr("id")) {
-					case options.filterTypeElement.attr("id"):
-						$(value).val(state.type);
-						break;
-					case options.filterStatusElement.attr("id"):
-						$(value).val(state.status);
-						break;
-				}
-			});
-		};
-
-		// reload the service request items view for the current state
-		this.history.showHistory = function (historyState) {
-	console.log(historyState)
-			// set selected elements on filters in case change is because of URL hash edit, rather than UI interaction (SET SELECTED YEAR OMITTED FOR THIS EXAMPLE)
-			history.setFilterState(historyState);
-			// here would be the ajax call for the history list view (html), just static html for this example
-			var htmlViewFromAjaxCall = "<h2>Service Requests for Year: " + historyState.year + " Type: " + historyState.type + " Status: " + historyState.status + "</h2>";
-			options.historyElement.html(htmlViewFromAjaxCall);
-		};
-
+var requestHistory = (typeof (requestHistory) == "undefined" || !requestHistory)
+ ? function () {
+	return {
+		history: this.history
+	};
+} : requestHistory;
+/**
+ *
+ */
+requestHistory.options = (typeof (requestHistory.options) == "undefined" || !requestHistory.options)
+ ? {} : requestHistory.options;
+/**
+ *
+ */
+requestHistory.history = (typeof (requestHistory.history) == "undefined" || !requestHistory.history)
+ ? function (options) {
 		// initialize the yearPicker component (was DateManager)
 		options.dateElement.yearPicker({
 			'selectedYear': options.state.year,
 			'yearsToShow': options.yearsToShow,
-			'onChange': history.onYearChange // event handler to call when year selection changes
+			'onChange': requestHistory.history.onYearChange // event handler to call when year selection changes
 		});
+		requestHistory.options = options;
 
 		// set initially selected values for filters, and the event handler for filter changes
-		history.setFilterState(options.state);
+		requestHistory.history.setFilterState(options.state);
 		options.filtersElement.find("select").each(function (index, value) {
 			$(value).change(function () {
 				history.onFilterChange($(value));
 			});
 		});
-
-	};
-
-	// return public history interface
-	return {
-		history: this.history
-	};
-})(jQuery);
+} : requestHistory.history;
+/**
+ * Clicking a Year triggers this method
+ */
+requestHistory.history.onYearChange = (typeof (requestHistory.history.onYearChange) == "undefined" || !requestHistory.history.onYearChange)
+ ? function (year) {
+		$.bbq.pushState({ "year": year });
+} : requestHistory.history.onYearChange;
+/*
+*/
+requestHistory.history.onFilterChange = (typeof (requestHistory.history.onFilterChange) == "undefined" || !requestHistory.history.onFilterChange)
+ ? function (filter) {
+		var val = filter.children("option:selected").val();
+		switch (filter.attr("id")) {
+			case requestHistory.options.filterTypeElement.attr("id"):
+				$.bbq.pushState({ "type": val });
+				break;
+			case requestHistory.options.filterStatusElement.attr("id"):
+				$.bbq.pushState({ "status": val });
+				break;
+		}
+} : requestHistory.history.onFilterChange;
+/*
+*/
+requestHistory.history.setFilterState = (typeof (requestHistory.history.setFilterState) == "undefined" || !requestHistory.history.setFilterState)
+ ? function (state) {
+	requestHistory.options.filtersElement.children("select").each(function (index, value) {
+		switch ($(value).attr("id")) {
+			case requestHistory.options.filterTypeElement.attr("id"):
+				$(value).val(state.type);
+				break;
+			case requestHistory.options.filterStatusElement.attr("id"):
+				$(value).val(state.status);
+				break;
+		}
+	})
+} : requestHistory.history.setFilterState;
+/*
+*/
+requestHistory.history.showHistory = (typeof (requestHistory.history.showHistory) == "undefined" || !requestHistory.history.showHistory)
+ ? function (historyState) {
+	console.log(historyState)
+	// set selected elements on filters in case change is because of URL hash edit, rather than UI interaction (SET SELECTED YEAR OMITTED FOR THIS EXAMPLE)
+	requestHistory.history.setFilterState(historyState);
+	// here would be the ajax call for the history list view (html), just static html for this example
+	var htmlViewFromAjaxCall = "<h2>Service Requests for Year: " + historyState.year + " Type: " + historyState.type + " Status: " + historyState.status + "</h2>";
+	requestHistory.options.historyElement.html(htmlViewFromAjaxCall);
+} : requestHistory.history.showHistory;
