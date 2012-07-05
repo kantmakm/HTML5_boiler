5fe33efe6aadb0a3c8a7c06405267a40ba9e52a6
Mad Jon <biting_duck@hotmail.com>
2012-07-05 10:58:16 -0600

    contactInfo

--- js/contact-information.js
+++ js/contact-information.js
@@ -3,10 +3,15 @@ var accounts = (typeof (accounts) === "undefined" || !accounts) ? {} : accounts;
 
 accounts.switchable = function()
 {
+	console.log('switchable');
 	$('.squared-box h1 .submit').toggle(function(){
 		$('.switchable').addClass('switchable-edit');
+		$('.switchable li p').fadeOut();
+		$('.switchable li:hidden').show('blinds');
 	}, function(){
 		$('.switchable').removeClass('switchable-edit');
+		$('.switchable li p').fadeIn();
+		$('.switchable li input[value=""]').parents('li').hide('blinds');
 	});
 };
 
