32391a532345ddb21b06d989acf25fe8ba190c8f
Mad Jon <biting_duck@hotmail.com>
2012-07-10 14:55:07 -0600

    bug#http://eventum.spiremedia.com/view.php?id=2380

--- js/plugins.js
+++ js/plugins.js
@@ -10,3 +10,78 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
 // $.fn.multiAccordion = function();
+
+/**
+ * Duplicatable sections 
+ */
+$.fn.duplicatable = function(method)
+{
+	// methods to call
+	var methods = {
+		/* Init */
+		init: function(options){
+			console.log('hey!');
+			var $this = $(this);
+			$this.duplicatable.settings = $.extend({
+				'addButton': 			$(this).find('.add'),
+				'removeButton':		$(this).find('.remove'),
+				'controls': 			$(this).find('li.controls'),
+				'templateCloneFilter': function(clone){
+					clone.find('input').val('');
+					clone.find('ul li span').remove();
+					clone.find('.input-validation-error').removeClass('input-validation-error');
+					clone.find('select');
+				}
+			}, options);
+			$this.duplicatable.settings.templateClone = $this.children('li').not($this.duplicatable.settings.controls).eq(0);
+			$this.duplicatable.settings.templateCloneFilter($this.duplicatable.settings.templateClone);
+			
+			$this.duplicatable.settings.addButton.click(function(){
+				$this.duplicatable('add');
+				return false;
+			});
+			$this.duplicatable.settings.removeButton.click(function(){
+				$this.duplicatable('remove');
+				return false;
+			});
+		},
+		add: function(){
+			var $this = $(this);
+			console.log($(this));
+			$last = $this.children('li').not($this.duplicatable.settings.controls).last();
+			console.log($last);
+			$this.duplicatable.settings.templateClone.clone(true, true).hide().insertAfter($last).show('blind');
+			console.log('?yeah');
+			if($this.children('li').not($this.duplicatable.settings.controls).length > 0)
+			{
+				$this.duplicatable.settings.removeButton.show('blind');
+			}
+			return false;
+		},
+		remove: function(){
+			var $this = $(this);
+			if($this.children('li').not($this.duplicatable.settings.controls).length < 3)
+			{
+				$this.duplicatable.settings.removeButton.hide('blind');
+			}
+			$this.children('li').not($this.duplicatable.settings.controls).last().hide('blind', function(){
+				$(this).remove();
+			});
+		}
+	};
+	
+	/**
+	 * Method calling logic
+	 */
+  if ( methods[method] ) {
+    return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+  } else if ( typeof method === 'object' || ! method ) {
+    return methods.init.apply( this, arguments );
+  } else {
+    $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+  }
+};
+$(function(){
+	console.log($('ul.duplicatable').length);
+	if($('ul.duplicatable').length > 0)$('ul.duplicatable').duplicatable();
+});
